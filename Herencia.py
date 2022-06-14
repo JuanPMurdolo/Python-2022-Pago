65e1287513ef227fee673ad243af5bb73c672397
Juan P Murdolo <40174725+JuanPMurdolo@users.noreply.github.com>
2022-06-13 21:15:13 -0300

    Update Herencia.py

--- Herencia.py
+++ Herencia.py
@@ -1,3 +1,14 @@
+'''}
+                        -> Coche(velocidad, cilindrada) -> Camioneta(Carga)
+vehiculo (color, ruedas)
+                        ->Bicicleta(tipo(Urbana/Deportiva)) -> Motocicleta(velocidad, cilindrada)
+Experimenta
+Crea al menos un objeto de cada subclase y añádelos a una lista llamada vehiculos.
+Realiza una función llamada catalogar() que reciba la lista de vehiculos y los recorra mostrando el nombre de su clase y sus atributos.
+Modifica la función catalogar() para que reciba un argumento optativo ruedas, haciendo que muestre únicamente los que su número de ruedas concuerde con el valor del argumento. También debe mostrar un mensaje "Se han encontrado {} vehículos con {} ruedas:" únicamente si se envía el argumento ruedas. Ponla a prueba con 0, 2 y 4 ruedas como valor.
+Recordatorio: Puedes utilizar el atributo especial de clase name de la siguiente forma para recuperar el nombre de la clase de un objeto:
+'''
+
 class Vehiculo():
     
     def __init__(self, color, ruedas):
