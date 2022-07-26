7555418a7ffade65eb1dc5d5dcb613c38b90d67d
Juan P Murdolo <40174725+JuanPMurdolo@users.noreply.github.com>
2022-07-26 18:18:28 -0300

    Clase 182

--- sqlite/Leccion1.py
+++ sqlite/Leccion1.py
@@ -3,6 +3,25 @@ import sqlite3
 conexion = sqlite3.connect("ejemplo.db")
 cursor = conexion.cursor()
 #cursor.execute("CREATE TABLE usuarios (nombre VARCHAR(50), edad INTEGER, email VARCHAR(100))")
-cursor.execute("INSERT INTO usuarios VALUES ("Juan Pablo",30,"ejemplo@gmail.com")")
+#cursor.execute("INSERT INTO usuarios VALUES ('Juan Pablo',30,'ejemplo@gmail.com')")
+#cursor.execute("SELECT * FROM usuarios")
+#print(cursor)
+#usuario = cursor.fetchone()
+#print(usuario)
+
+'''usuarios = [
+    ('Juan', 20, 'alfa@gmail.com'),
+    ('Pablo', 30, 'alfa@gmail.com'),
+    ('Martin', 40, 'alfa@gmail.com'),
+]'''
+
+#cursor.executemany("INSERT INTO usuarios VALUES (?,?,?)", usuarios)
+
+cursor.execute("SELECT * FROM usuarios")
+usuarios = cursor.fetchall()
+for usuario in usuarios:
+    print(usuario)
+
+conexion.commit()
 
 conexion.close()
