7ae68f0f96bd3723202e06862656632cd270c478
Juan P Murdolo <40174725+JuanPMurdolo@users.noreply.github.com>
2022-08-03 17:40:52 -0300

    Se cambio todo lo csv por Sqlite3

--- ProyectoFinal/config.py
+++ ProyectoFinal/config.py
@@ -1,6 +1,9 @@
 import sys
+import sqlite3
 
 DATABASE_PATH = "clientes.csv"
+CONEXION = sqlite3.connect("clientes.db")
+CURSOR = CONEXION.cursor()
 
 if "pytest" in sys.argv[0]:
     DATABASE_PATH = "tests/clientes_test.csv"
--- ProyectoFinal/database.py
+++ ProyectoFinal/database.py
@@ -13,23 +13,40 @@ class Cliente:
 class Clientes:
 
     lista = []
+    conexion = config.CONEXION
+    cursor = conexion.cursor()
+    '''
     with open(config.DATABASE_PATH) as fichero:
         reader = csv.reader(fichero, delimiter=';')
         for dni, nombre, apellido in  reader:
             cliente = Cliente(dni, nombre, apellido)
             lista.append(cliente)
-
+    '''
+    cursor.execute("SELECT * FROM clientes")
+    clientes = cursor.fetchall()
+    cli = Cliente(None, None, None)
+    for cliente in clientes:
+        cli.dni = cliente[0]
+        cli.nombre = cliente[1]
+        cli.apellido = cliente[2]
+        lista.append(cli)
+    cursor.close()
+    
     @staticmethod
     def buscar(dni):
-        for cliente in Clientes.lista:
-            if cliente.dni == dni:
-                return cliente
+        conexion = config.CONEXION
+        cursor = conexion.cursor()
+        #for cliente in Clientes.lista:
+        #    if cliente.dni == dni:
+        #        return cliente
+        cursor.execute("SELECT * FROM clientes WHERE dni={dni}")
+        cursor.close()
 
     @staticmethod
     def crear(dni, nombre, apellido):
         cliente = Cliente(dni, nombre, apellido)
         Clientes.lista.append(cliente)
-        Clientes.guardar()
+        Clientes.guardar(cliente)
         return cliente
     
     @staticmethod
@@ -38,7 +55,9 @@ class Clientes:
             if cliente.dni == dni:
                 Clientes.lista[indice].nombre = nombre
                 Clientes.lista[indice].apellido = apellido
-                Clientes.guardar()
+                conexion = config.CONEXION
+                cursor = conexion.cursor()
+                cursor.execute("UPDATE clientes SET nombre='{}', apellido='{}' WHERE dni='{}'".format(nombre, apellido, dni))
                 return Clientes.lista[indice]
 
     @staticmethod
@@ -46,12 +65,20 @@ class Clientes:
         for indice,cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
                 cliente = Clientes.lista.pop(indice)
-                Clientes.guardar()
+                conexion = config.CONEXION
+                cursor = conexion.cursor()
+                cursor.execute("DELETE FROM clientes WHERE dni='{}'".format(dni))
+                conexion.commit()
                 return cliente
 
     @staticmethod
-    def guardar():
-        with open(config.DATABASE_PATH, 'w') as fichero:
-            writer = csv.writer(fichero, delimiter=";")
-            for cliente in Clientes.lista:
-                writer.writerow((cliente.dni, cliente.nombre, cliente.apellido))
+    def guardar(cliente):
+        conexion = config.CONEXION
+        cursor = conexion.cursor()
+        #with open(config.DATABASE_PATH, 'w') as fichero:
+        #    writer = csv.writer(fichero, delimiter=";")
+        #    for cliente in Clientes.lista:
+        #        writer.writerow((cliente.dni, cliente.nombre, cliente.apellido))
+        cursor.execute("INSERT INTO clientes VALUES ('{}','{}','{}')".format(cliente.dni, cliente.nombre, cliente.apellido))
+        conexion.commit()
+        cursor.close()
